a9b826eea69f9d6a88ce16853b8c5b8db225e558
Sylvain Munaut <tnt@246tNt.com>
2021-01-16 15:25:34 +0100

    src/m_fixed: Definitely don't want to pull in 'double' !
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/m_fixed.c
+++ src/m_fixed.c
@@ -72,12 +72,11 @@ FixedDiv2
 ( fixed_t       a,
   fixed_t       b )
 {
-#if 0
+#if 1
     long long c;
     c = ((long long)a<<16) / ((long long)b);
     return (fixed_t) c;
-#endif
-
+#else
     double c;
 
     c = ((double)a) / ((double)b) * FRACUNIT;
@@ -85,4 +84,5 @@ FixedDiv2
     if (c >= 2147483648.0 || c < -2147483648.0)
         I_Error("FixedDiv: divide by zero");
     return (fixed_t) c;
+#endif
 }
